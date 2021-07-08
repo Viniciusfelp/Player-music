7e4e10e36226f52500b4516dad2a5a1252c4e544
Viniciusfelp <56048036+Viniciusfelp@users.noreply.github.com>
2021-07-08 13:43:14 -0300

    inicio

---
+++ src/music.java
@@ -0,0 +1,30 @@
+public class music {
+    private String nomeMusica;
+    private String nomeArtista;
+    private int duracaoMusica;
+
+    public String getNomeMusica() {
+        return nomeMusica;
+    }
+
+    public void setNomeMusica(String nomeMusica) {
+        this.nomeMusica = nomeMusica;
+    }
+
+    public String getNomeArtista() {
+        return nomeArtista;
+    }
+
+    public void setNomeArtista(String nomeArtista) {
+        this.nomeArtista = nomeArtista;
+    }
+
+    public int getDuracaoMusica() {
+        return duracaoMusica;
+    }
+
+    public void setDuracaoMusica(int duracaoMusica) {
+        this.duracaoMusica = duracaoMusica;
+    }
+
+}
