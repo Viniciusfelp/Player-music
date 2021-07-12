dde98ae5f994ea46cd2a3426608b5a37a94aa475
Viniciusfelp <56048036+Viniciusfelp@users.noreply.github.com>
2021-07-12 07:43:02 -0300

    atualizações

--- src/player.java
+++ src/player.java
@@ -12,20 +12,19 @@ public class player {
         for(int i = 0; i < listaDeMusicas.lista.length; i++ ){
             System.out.println(i+"->"+listaDeMusicas.getNomeMusica(i));
         }
-        while(s.hasNext()){
-            System.out.println("1- Adicionar musica na playList:\n2- Remover Musica da playList: ");
-            int opc = s.nextInt();
-            if(opc == 1){
-                System.out.println("Digite o número da música para adicionar à Playlist:");
-                int mEscolhido = s.nextInt();
-                Thread add = new Thread (new adicionar(mEscolhido));
-                add.start();
-            }else if(opc==2){ //remover músicas
-                System.out.println("Digite o número da música para remover da Playlist:");
-                int mEscolhido = s.nextInt();
-                Thread remove = new Thread(new remover(mEscolhido));
-                remove.start();
-            }
+        System.out.println("1- Adicionar musica na playList:\n2- Remover Musica da playList: ");
+        int opc = s.nextInt();
+        if(opc == 1){
+            System.out.println("Digite o número da música para adicionar à Playlist:");
+            int mEscolhido = s.nextInt();
+            Thread add = new Thread (new adicionar(mEscolhido));
+            add.start();
+        }else if(opc==2){ //remover músicas
+            System.out.println("Digite o número da música para remover da Playlist:");
+            int mEscolhido = s.nextInt();
+            Thread remove = new Thread(new remover(mEscolhido));
+            remove.start();
         }
     }
 }
+
